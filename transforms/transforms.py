933de7762376bed5d57cdd54d4226cc2db0d648d
Ayushee Mittal <ayusheemittal530@gmail.com>
2020-09-19 16:14:31 +0530

    Update transforms.py
    Added Random crop for regularization.

--- transforms/transforms.py
+++ transforms/transforms.py
@@ -4,7 +4,8 @@ import torchvision.transforms as transforms
 
 def model7_transforms():
     transform = transforms.Compose(
-        [transforms.ToTensor(),
+        [transforms.RandomCrop(30, padding = 2),
+         transforms.ToTensor(),
          transforms.RandomHorizontalFlip(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     return transform
