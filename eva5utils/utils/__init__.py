92a1acd35a17fb9ca9405c707458de4e78272164
Ayushee Mittal <ayusheemittal530@gmail.com>
2020-10-02 23:11:37 +0530

    Update __init__.py

--- eva5utils/utils/__init__.py
+++ eva5utils/utils/__init__.py
@@ -2,3 +2,4 @@ from .helpers import show_model_summary, DEVICE, IS_CUDA, show_gradcam
 from .plotting import plot_samples
 from .lr_finder import  LRFinder, TrainDataLoaderIter, ValDataLoaderIter
 from .gradcam import GradCAM
+from .gradcam_utils import visualize_cam
