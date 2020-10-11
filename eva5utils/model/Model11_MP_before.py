d5720304171bd04d97136c6c5df12863815f6ad0
smitasasindran <smita.sasindran@glassbeam.com>
2020-10-12 00:15:27 +0530

    Pending commit - updated Model11 + backup of old Model11

--- eva5utils/model/Model11_MP_before.py
+++ eva5utils/model/Model11_MP_before.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 import torch
 
 
-class Model11(nn.Module):
+class Model11_MP_before(nn.Module):
     def __init__(self):
         super(Model11, self).__init__()
 
@@ -18,9 +18,9 @@ class Model11(nn.Module):
         # X = Conv 3x3 (s1, p1) >> MaxPool2D >> BN >> RELU [128k]
         self.x1 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=1, padding=1, bias=False),
+            nn.MaxPool2d(2, 2),
             nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2)
+            nn.ReLU()
         )
         # R1 = ResBlock( (Conv-BN-ReLU-Conv-BN-ReLU))(X) [128k]
         self.r1 = nn.Sequential(
@@ -35,18 +35,18 @@ class Model11(nn.Module):
         # Layer 2
         self.l2 = nn.Sequential(
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), stride=1, padding=1, bias=False),
+            nn.MaxPool2d(2, 2),
             nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2)
+            nn.ReLU()
         )
 
         # Layer 3
         # X2 = Conv 3x3 (s1, p1) >> MaxPool2D >> BN >> RELU [512k]
         self.x2 = nn.Sequential(
             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3, 3), stride=1, padding=1, bias=False),
+            nn.MaxPool2d(2, 2),
             nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2)
+            nn.ReLU()
         )
 
         # R2 = ResBlock( (Conv-BN-ReLU-Conv-BN-ReLU))(X) [512k]
--- eva5utils/model/__init__.py
+++ eva5utils/model/__init__.py
@@ -1,3 +1,4 @@
 from .Model7 import *
 from .ResNet import *
 from .Model11 import *
+from .Model11_MP_before import *
